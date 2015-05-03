cfd40497f2bf7caf393d6d5a2748f89180193a19
Brennan Harris <brennanharris@Brennans-MacBook-Pro.local>
2015-05-02 22:19:33 -0600

    made irisReader do real machine learning on iris dataset and brest cancer dataset

--- irisReader.py
+++ irisReader.py
@@ -12,12 +12,24 @@
 #======================================#
 
 import csv
+import math
 from random import shuffle
+from operator import itemgetter
+from collections import Counter
+#from sklearn import neighbors
 
 class analyzeIris():
 
+    def __init__(self):
+        self.list =[]
+        self.learnSet = []
+        self.testSet = []
+        self.correctCount = 0
+        self.falseCount = 0
+
     def menu(self):
         self.file = raw_input("enter the csv filename: ")
+        self.classPosition = int(raw_input("enter the row position of the classification [1 for 1st, 2 for 2nd, etc.]: "))
 
     def read(self):
 
@@ -25,17 +37,12 @@ class analyzeIris():
 
             dater = csv.reader(iris_data)
 
-            self.list =[]
-
             for thing in dater:
                 self.list.append(thing)
 
             shuffle(self.list)
 
     def split(self):
-
-        self.learnSet = []
-        self.testSet = []
 
         length = len(self.list)
 
@@ -58,17 +65,45 @@ class analyzeIris():
             except:
                 pass
 
-    def learn(self):
+    def getEucDist(self, inst1, inst2, length=4):
+        distance = 0
+        for i in range(length):
+            try:
+                distance += pow((float(inst1[i]) - float(inst2[i])), 2)
+            except:
+                pass
+        return math.sqrt(distance)
 
-        self.learnedVal = 'Iris-setosa'
+    def neighbors(self,testInst):
 
-    def guess(self):
+        k = 3
+        distances = []
+        length = len(testInst)-1
+        for i in range(1,len(self.learnSet)):
+            dist = self.getEucDist(testInst,self.learnSet[i],length)
+            distances.append((self.learnSet[i],dist))
+        distances.sort(key=itemgetter(1))
+        neighbors = []
+        for i in range(k):
+            neighbors.append(distances[i][0])
+        return neighbors
 
-        self.correctCount = 0
-        self.falseCount = 0
+    def guess(self,neighbors):
+
+        list = []
+        for i in neighbors:
+            list.append(i[self.classPosition - 1])
+
+        classification = Counter(list)
+        mode = classification.most_common(1)[0][0]
+        print mode
+        return mode
+
+    def analyze(self):
 
         for i in self.testSet:
-            if self.learnedVal in i[4]:
+            guess = self.guess(self.neighbors(i))
+            if guess in i[self.classPosition - 1]:
                 self.correctCount += 1
             else:
                 self.falseCount += 1
@@ -77,14 +112,10 @@ class analyzeIris():
 
         self.percent = int(100 * (float(self.correctCount) / float(self.total)))
 
-    def analyze(self):
-
         print "The learned value was accurate for "+ str(self.percent) +"% of the test data"
 
 start = analyzeIris()
 start.menu()
 start.read()
 start.split()
-start.learn()
-start.guess()
 start.analyze()
